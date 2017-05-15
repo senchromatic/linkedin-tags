18e171c0b35f8b160d38f9c958628e566c93e967
senchromatic <samuelfzhang@gmail.com>
2017-05-15 15:16:20 -0400

    init

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install python2.7
+sudo apt-get install python-dev
+sudo apt-get install python-pip 
+sudo pip install bs4
