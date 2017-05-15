bf9fee0a77f15d201651209af2a9cb9269c716a1
senchromatic <samuelfzhang@gmail.com>
2017-05-15 17:12:59 -0400

    encapsulation

--- google_search.py
+++ google_search.py
@@ -1,25 +1,25 @@
-import requests
 from bs4 import BeautifulSoup
+import requests
 
-BASE_URL = 'https://www.google.com/search?q=site%3Alinkedin.com+'
-QUERY = '"algorithmic+trader"'
-START_NAME = '&start='
-USER_SUBDOMAIN = 'linkedin.com/in/'
 
 def get_soup(url):
-	html = requests.get(url)
-	return BeautifulSoup(html.content, 'lxml') 
+	html = requests.get(url).content
+	return BeautifulSoup(html, 'lxml') 
 
-def get_profile_urls(query, start):
-	start_value = str(start)
-	soup = get_soup(BASE_URL + query + START_NAME + start_value)
-	results = soup.findAll('div', {'class':'g'})
-	urls = []
-	for result in results:
-		link = result.find('cite')
-		url = link.text
-		if USER_SUBDOMAIN in url:
-			urls.append(url)
-	return urls
+class Google:
+	BASE_URL = 'https://www.google.com/search?q=site%3Alinkedin.com+'
+	START_NAME = '&start='
+	USER_SUBDOMAIN = 'linkedin.com/in/'
 
-# urls = get_profile_urls(QUERY, 10)
+	@staticmethod	
+	def get_profile_urls(query, start):
+		start_value = str(start)
+		soup = get_soup(Google.BASE_URL + query + Google.START_NAME + start_value)
+		results = soup.findAll('div', {'class':'g'})
+		urls = []
+		for result in results:
+			link = result.find('cite')
+			url = link.text
+			if Google.USER_SUBDOMAIN in url:
+				urls.append(url)
+		return urls
