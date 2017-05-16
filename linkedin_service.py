91e1b94859fcefcbe317c910ab19f7e40707f389
senchromatic <samuelfzhang@gmail.com>
2017-05-16 14:45:20 -0400

    retry in case of network failure, smoother scrolling

--- linkedin_service.py
+++ linkedin_service.py
@@ -12,7 +12,7 @@ class LinkedIn:
 	WELCOME_TEXT = 'Welcome, '
 	MAX_TIMEOUT = 60
 	SAFE_DELAY = 5.0
-	MICROSLEEPS = 10
+	MICROSLEEPS = 100
 	
 	def __init__(self):
 		self.browser = webdriver.Firefox()
@@ -20,19 +20,25 @@ class LinkedIn:
 	
 	def wait(self, condition, scroll=False):
 		if not condition:
-			return
+			return True
 		ready = EC.presence_of_element_located(condition)
-		WebDriverWait(self.browser, LinkedIn.MAX_TIMEOUT).until(ready)
-		for microsleep in xrange(1, 1+LinkedIn.MICROSLEEPS):
-			if scroll:
-				fraction = str(microsleep) + '/' + str(LinkedIn.MICROSLEEPS)
-				scroll_script = 'window.scrollTo(0, document.body.scrollHeight*' + fraction + ');'
-				self.browser.execute_script(scroll_script)
-			sleep(LinkedIn.SAFE_DELAY / LinkedIn.MICROSLEEPS)
+		try:
+			WebDriverWait(self.browser, LinkedIn.MAX_TIMEOUT).until(ready)
+			for microsleep in xrange(1, 1+LinkedIn.MICROSLEEPS):
+				if scroll:
+					fraction = str(microsleep) + '/' + str(LinkedIn.MICROSLEEPS)
+					scroll_script = 'window.scrollTo(0, document.body.scrollHeight*' + fraction + ');'
+					self.browser.execute_script(scroll_script)
+				sleep(LinkedIn.SAFE_DELAY / LinkedIn.MICROSLEEPS)
+			return True
+		except TimeOutException as exception:
+			return False
 	
 	def load(self, url, condition=None, scroll=False):
-		self.browser.get(url)
-		self.wait(condition, scroll)
+		successful = None
+		while not successful:
+			self.browser.get(url)
+			successful = self.wait(condition, scroll)
 	
 	def make_soup(self, url, condition=None, scroll=False):
 		self.load(url, condition, scroll)
