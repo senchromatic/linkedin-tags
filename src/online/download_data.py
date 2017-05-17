32375d1528c0fae92b8498320404e052bd9cb716
senchromatic <samuelfzhang@gmail.com>
2017-05-17 13:03:41 -0400

    pythonic quotes consistency

--- src/online/download_data.py
+++ src/online/download_data.py
@@ -4,7 +4,7 @@ def prompt(retriever):
 	retriever.query = raw_input('Please query an occupational role, e.g. "software+engineer": ')
 	return retriever.query
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	retriever = Retriever()
 	while prompt(retriever):
 		retriever.download_all_data()
