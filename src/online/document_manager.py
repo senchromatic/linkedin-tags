5cc20e47d60ae2fcc9f49c6cc9b57041d0c48022
senchromatic <samuelfzhang@gmail.com>
2017-05-17 12:48:42 -0400

    sync

---
+++ src/online/document_manager.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+import sys
+
+class Database:
+	OUTPUT_DIRECTORY = "data/"
+	
+	@staticmethod
+	def setup_encoding():
+		reload(sys)
+		sys.setdefaultencoding('utf-8')
+	
+	@staticmethod
+	def tokenize(dom):
+		return dom.text.split()
+	
+	@staticmethod
+	def save_local(tokens, query):
+		plus_to_underscore = re.sub(r'\+', '_', query)
+		only_word_characters = re.sub(r'\W+', '', plus_to_underscore)
+		filename = only_word_characters + '.txt'
+		path = Database.OUTPUT_DIRECTORY + filename
+		with open(path, 'a') as output:
+			output.write(' '.join(tokens) + '\n')
