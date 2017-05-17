0696eb167ee045539d263729d4704664f7597bf3
senchromatic <samuelfzhang@gmail.com>
2017-05-17 19:08:25 -0400

    statsmodels

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ sudo apt-get install build-essential
 sudo apt-get install python2.7
 sudo apt-get install python-dev
 sudo apt-get install python-pip 
-sudo pip install selenium
 sudo pip install bs4
+sudo pip install selenium
+sudo pip install statsmodels
